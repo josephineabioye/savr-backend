0a0960ce94a2e19b0dbc7409328a08e0ba5e6268
josephineabioye <josephineabioye@yahoo.com>
2026-01-31 21:55:05 +0000

    update AccountService class

--- src/main/java/savr/service/AccountService.java
+++ src/main/java/savr/service/AccountService.java
@@ -1,13 +1,16 @@
 package savr.service;
 
+import org.springframework.stereotype.Service;
 import savr.model.Account;
 import savr.model.Pocket;
 
 import java.util.List;
-
+@Service
 public class AccountService {
-    private Account account = new Account("Main", 0);
-
+    private Account account;
+    public AccountService() {
+    this.account  = new Account("Main", 0);
+    }
     public void deposit(double amount) {
         account.deposit(amount);
     }
