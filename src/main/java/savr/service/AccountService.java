b50c33b9428dd4c8724bcc0c8201930bc945962b
josephineabioye <josephineabioye@yahoo.com>
2026-01-31 20:47:00 +0000

    update AccountService class

--- src/main/java/savr/service/AccountService.java
+++ src/main/java/savr/service/AccountService.java
@@ -1,4 +1,20 @@
 package savr.service;
 
+import savr.model.Account;
+import savr.model.Pocket;
+
+import java.util.List;
+
 public class AccountService {
+    private Account account = new Account("Main", 0);
+
+    public void deposit(double amount) {
+        account.deposit(amount);
+    }
+    public double getBalance() {
+        return account.getAccountBalance();
+    }
+    public List<Pocket> getPockets() {
+        return account.pocketListName();
+    }
 }
