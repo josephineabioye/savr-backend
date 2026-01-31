69e528649118c691ee84be8c0c13c75d8189cab4
josephineabioye <josephineabioye@yahoo.com>
2026-01-31 20:59:08 +0000

    update AccountController class

--- src/main/java/savr/controller/AccountController.java
+++ src/main/java/savr/controller/AccountController.java
@@ -1,4 +1,24 @@
 package savr.controller;
 
+import org.springframework.web.bind.annotation.*;
+import savr.service.AccountService;
+
+@RestController
+@RequestMapping("/api/account")
+
 public class AccountController {
+    private final AccountService accountService;
+
+    public AccountController(AccountService acctService) {
+        accountService = acctService;
+    }
+    @PostMapping("/deposit")
+    public String deposit(@RequestParam double amount) {
+        accountService.deposit(amount);
+        return "Deposited " + amount;
+    }
+    @GetMapping("/balance")
+    public double balance() {
+        return accountService.getBalance();
+    }
 }
