29bbc36c4739739dc1c780f0a522efff2957077c
josephineabioye <josephineabioye@yahoo.com>
2026-02-03 23:43:45 +0000

    update TransactionRepository class

--- src/main/java/savr/repository/TransactionRepository.java
+++ src/main/java/savr/repository/TransactionRepository.java
@@ -1,4 +1,9 @@
 package savr.repository;
 
-public class TransactionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import savr.model.Transaction;
+import java.util.List;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findAllByOrderByCreatedAtDesc();
 }
