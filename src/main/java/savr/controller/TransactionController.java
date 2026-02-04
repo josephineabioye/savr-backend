d78169c81e5a934ac449983f74244e9aa6d3edf3
josephineabioye <josephineabioye@yahoo.com>
2026-02-04 00:15:41 +0000

    update TransactionController class

--- src/main/java/savr/controller/TransactionController.java
+++ src/main/java/savr/controller/TransactionController.java
@@ -1,4 +1,21 @@
 package savr.controller;
 
+import org.springframework.web.bind.annotation.*;
+import savr.model.Transaction;
+import savr.service.TransactionService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/transactions")
 public class TransactionController {
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+    @GetMapping("/history")
+    public List<Transaction> getTransactionHistory() {
+        return transactionService.getAllTransactions();
+    }
 }
