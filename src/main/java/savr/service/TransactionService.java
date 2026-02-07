d34a5bfa7115575f399c1d647a56f7469a7e64c8
josephineabioye <josephineabioye@yahoo.com>
2026-02-07 23:16:46 +0000

    update to classes

--- src/main/java/savr/service/TransactionService.java
+++ src/main/java/savr/service/TransactionService.java
@@ -13,7 +13,7 @@ public class TransactionService {
     public TransactionService(TransactionRepository transactionRepository) {
         this.transactionRepository = transactionRepository;
     }
-    public void logTransaction(String type, String description, double amount) {
+    public void logTransaction(String type, double amount, String description) {
         Transaction tx = new Transaction(type, amount, description);
         tx.setTransactionType();
         tx.setTransactionAmount();
@@ -22,9 +22,7 @@ public class TransactionService {
         }
         transactionRepository.save(tx);
     }
-    public void logTransaction(String type, double amount, String description) {
-        Transaction tx = new Transaction(description, amount, type);
-        transactionRepository.save(tx);
+
     }
     public void logTransaction(String description, double amount ) {
         Transaction tx = new Transaction("DEPOSIT", amount, description);
